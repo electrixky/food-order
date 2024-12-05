c22c615ade71f5ea06c296956e9516e6e47582f5
electrixky <tanyatkachuk2401@gmail.com>
2024-12-05 12:38:38 +0200

    navbar styled

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,12 +2,21 @@ import React from 'react';
 
 const Navbar = () => {
     return (
-        <nav>
+        <nav className="flex flex-col lg:flex-row justify-between py-3 mx-6">
             <div>
-                <h3>{new Date().toUTCString().slice(0, 16)}</h3>
-                <h1>Food Order</h1>
+                <h3 className="text-xl font-bold text-gray-600">{new Date().toUTCString().slice(0, 16)}</h3>
+                <h1 className="text-2xl font-bold ">Food Order</h1>
             </div>
-            <div></div>
+            <div>
+                <input
+                    type="search"
+                    name="search"
+                    id=""
+                    placeholder="Search here..."
+                    autoComplete="off"
+                    className="p-3 border border-gray-400 text-sm rounded-lg outline-none w-full lg:w-[25vw]"
+                />
+            </div>
         </nav>
     );
 };
