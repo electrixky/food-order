e0ef2631610e0acb5770a18cb8bd782463bd11c9
electrixky <tanyatkachuk2401@gmail.com>
2024-12-05 12:55:48 +0200

    category buttons

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from "../components/Navbar.jsx";
+import CategoryMenu from "../components/CategoryMenu.jsx";
 
 const Home = () => {
     return (
         <>
             <Navbar/>
+            <CategoryMenu/>
         </>
     );
 };
