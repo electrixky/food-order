2d366098c1094f487f8b5dcbf354332fdc6f59f8
electrixky <tanyatkachuk2401@gmail.com>
2024-12-08 16:00:07 +0200

    food card styled

--- src/components/FoodCard.jsx
+++ src/components/FoodCard.jsx
@@ -9,15 +9,18 @@ const FoodCard = () => {
                 alt="Onion Pizza"
                 className="w-auto h-[130px] hover:scale-110 cursor-grab transition-all duration-500 ease-in-out"
             />
-            <div>
+            <div className="text-sm flex justify-between">
                 <h2>Onion Pizza</h2>
-                <span>₴150</span>
+                <span className="text-green-500">₴150</span>
             </div>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, pariatur.</p>
-            <div>
-                <span>
-                    <AiFillStar/> 4.5
+            <p className="text-sm font-normal">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam,
+                pariatur.</p>
+            <div className="flex justify-between">
+                <span className="flex justify-center items-center">
+                    <AiFillStar className="mr-1 text-yellow-400"/> 4.5
                 </span>
+                <button className="p-1 text-white bg-green-500 hover:bg-green-600 rounded-lg text-sm">Add to cart
+                </button>
             </div>
         </div>
     );
